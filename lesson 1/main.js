26d3ec9f527e791100a407771d4a8c0329be3378
haghir <47360153+haghir@users.noreply.github.com>
2021-09-19 17:51:08 +0900

    Added a triangle.

--- lesson 1/main.js
+++ lesson 1/main.js
@@ -53,6 +53,9 @@ window.onload =  function() {
         0, 0,
         0, 0.5,
         0.7, 0,
+        0, 0,
+        0, -0.5,
+        -0.7, 0,
     ];
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(position), gl.STATIC_DRAW);
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
@@ -60,17 +63,18 @@ window.onload =  function() {
     gl.clear(gl.COLOR_BUFFER_BIT);
     gl.useProgram(program);
     gl.enableVertexAttribArray(positionAttributeLocation);
-
-    var size = 2;          // 2 components per iteration
-    var type = gl.FLOAT;   // the data is 32bit floats
-    var normalize = false; // don't normalize the data
-    var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
-    var offset = 0;        // start at the beginning of the buffer
     gl.vertexAttribPointer(
-        positionAttributeLocation, size, type, normalize, stride, offset);
+        positionAttributeLocation,
+        2,        // 2 components per iteration
+        gl.FLOAT, // the data is 32bit floats
+        false,    // don't normalize the data
+        0,        // 0 = move forward size * sizeof(type) each iteration to get the next position
+        0         // start at the beginning of the buffer
+    );
 
-    var primitiveType = gl.TRIANGLES;
-    var offset = 0;
-    var count = 3;
-    gl.drawArrays(primitiveType, offset, count);
+    gl.drawArrays(
+        gl.TRIANGLES, // primitive type
+        0,            // offset
+        6             // count
+    );
 }
